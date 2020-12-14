7d1a4cbf7c962bc9f5279f24730b2a16a1625463
TakSeBiegam <63842313+TakSeBiegam@users.noreply.github.com>
2020-12-14 08:33:00 +0100

    Create Day_4_Part_1.cpp

---
+++ Day_4_Part_1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    fstream file;
+    string line;
+    char changer[] = " ";
+    char * chest;
+    int large_counter = 0;
+    int small_counter = 0;
+    file.open("data.txt", ios::in);
+    while(getline(file,line)){
+        if(line.empty()){
+            if(small_counter > 6){
+                large_counter++;
+            }
+            small_counter = 0;
+            continue;
+        }
+        else{     
+            int n = line.length();
+            char str[n];
+            strcpy(str, line.c_str());
+            chest = strtok(str, changer);
+            while( chest != NULL )
+            {
+                if(chest[0] == 'c'){
+                    chest = strtok( NULL, changer);
+                    continue;
+                }
+                small_counter++;
+                chest = strtok( NULL, changer );
+            }
+        }
+    }
+    
+    if(small_counter > 6){
+        large_counter++;
+    }
+
+    cout << large_counter << endl;
+
+    return 0;
+}
