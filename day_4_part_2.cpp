30041a5efc3f275b0b41b057d5a830bf21826dc4
TakSeBiegam <63842313+TakSeBiegam@users.noreply.github.com>
2020-12-15 21:11:22 +0100

    Update day_4_part_2.cpp

--- day_4_part_2.cpp
+++ day_4_part_2.cpp
@@ -1,144 +1,142 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <fstream> //fstream.file
+#include <string> //getline(file,line)
 #include <cstring>
-#include <unordered_map> 
-#include <sstream>
+#include <map>
+#include <sstream> //stringstream
 #include <vector>
 
 
 using namespace std;
 
-bool checker(unordered_map<string, string> ma){
-    vector<string> v;
-    //ints
-    v.push_back("byr");
-    v.push_back("iyr");
-    v.push_back("eyr");
-    //strings
-    v.push_back("hcl");
-    v.push_back("hgt");
-    v.push_back("ecl");
-    v.push_back("pid");
-    int int_data[3];
-    string string_data[4];
-    for(int i=0;i<3;i++){
-        istringstream(ma[v[i]]) >> int_data[i];
-    }
-    for(int i=0;i<4;i++){
-        string_data[i] = ma[v[i+3]];
-    }
-    if(int_data[0] < 1920 || int_data[0] > 2002){
+bool data_checker(map<string, string> &s_map, map<string, int> &i_map){
+    if(i_map["byr"] < 1920 || i_map["byr"] > 2002){
         return false;
     }
-    if(int_data[1] < 2010 || int_data[1] > 2020){
+    if(i_map["iyr"] < 2010 || i_map["iyr"] > 2020){
         return false;
     }
-    if(int_data[2] < 2020 || int_data[2] > 2030){
+    if(i_map["eyr"] < 2020 || i_map["eyr"] > 2030){
         return false;
     }
-    string temp = string_data[0];
-    if(string_data[0].size() != 7 || temp[0] != '#'){
+    if(s_map["pid"].size() != 9){
         return false;
     }
-    temp = string_data[1];
-    int temp_size = temp.size();
-    if(temp[temp_size-1] == 'n'){
-        string second_temp = "";
-        int h=0;
-        while(temp[h] != 'i'){
-            second_temp += temp[h];
-            h++;
+    if(s_map["hcl"].size() != 7 && s_map["hcl"][0] != '#'){
+        return false;
+    }
+    cout << "s_map: " << s_map["hgt"] << endl;
+    int hgt = s_map["hgt"].size();
+    cout << "hgt: " << hgt << endl;
+    string helper = "";
+    int tall = 0;
+    if(s_map["hgt"][hgt-1] == 'm'){
+        for(int i=0;i<(hgt-2);i++){
+            helper += s_map["hgt"][i];
         }
-        int tall_first;
-        istringstream(second_temp) >> tall_first;
-        if(tall_first < 59 || tall_first > 76){
+        istringstream(helper) >> tall;
+        cout << "tall: " << tall << endl;
+        if(tall < 150 || tall > 193){
             return false;
         }
     }
     else{
-        int h=0;
-        string second_temp = "";
-        while(temp[h] != 'c'){
-            second_temp += temp[h];
-            h++;
+        for(int i=0;i<(hgt-2);i++){
+            helper += s_map["hgt"][i];
         }
-        int tall_second;
-        istringstream(second_temp) >> tall_second;
-        if(tall_second < 150 || tall_second > 193){
+        istringstream(helper) >> tall;
+        cout << "tall: " << tall << endl;
+        if(tall < 59 || tall > 76){
             return false;
         }
     }
-    if(string_data[2] == "amb" || string_data[2] == "blu" || string_data[2] == "brn" || string_data[2] == "gry" || string_data[2] == "grn" || string_data[2] == "hzl" || string_data[2] == "oth"){
-        cout << "";
+    if(s_map["ecl"] == "amb" || s_map["ecl"] == "blu" || s_map["ecl"] == "brn" || s_map["ecl"] == "gry" || s_map["ecl"] == "grn" || s_map["ecl"] == "hzl" || s_map["ecl"] == "oth"){
+        return true;
+    }
+
+    return false;
+}
+
+bool data_distribution(vector<string> &vec){
+    int change_string;
+    map<string, string> string_map;
+    map<string, int> ints_map;
+    for(unsigned i=0 ; i < vec.size() ; i += 2){
+        if(vec[i] == "byr" || vec[i] == "iyr" || vec[i] == "eyr"){
+            stringstream geek(vec[i+1]);
+            geek >> change_string;
+            ints_map.insert(make_pair(vec[i], change_string));
+        }
+        else{
+            string_map.insert(make_pair(vec[i], vec[(i+1)]));
+        }
+    }
+    bool controller = data_checker(string_map, ints_map);
+    
+
+    ints_map.clear();
+    string_map.clear();
+    vec.clear();
+    if(controller == true){
+        return true;
     }
     else{
         return false;
     }
-    if(string_data[3].size() != 9){
-        return false;
+}
+
+void separate_pasport(fstream &f){
+    char changer[] = " :";
+    char * chest;
+    string line;
+    int large_counter = 0;
+    int small_counter = 0;
+    vector<string> temp_vec;
+    while(getline(f,line)){
+        if(line.empty()){
+            //cout << small_counter << endl;
+            if(small_counter > 6){
+                if(data_distribution(temp_vec) == true){
+                    large_counter++;
+                }
+            }
+            cout << endl << " NEW LINE " << endl;
+            small_counter = 0;
+            continue;
+        }
+        int n = line.length();
+        char str[n];
+        strcpy(str, line.c_str());
+        chest = strtok(str, changer);
+        while( chest != NULL ){
+            temp_vec.push_back(chest);
+            //cout << chest << endl;
+            if(chest[0] == 'c'){
+                chest = strtok( NULL, changer);
+                continue;
+            }
+            small_counter++;
+            chest = strtok( NULL, changer );
+        }
     }
-    return true;
+    if(small_counter > 6){
+        if(data_distribution(temp_vec) == true){
+            large_counter++;
+        }
+    }
+    cout << large_counter << endl;
 }
 
 int main()
 {
     fstream file;
-    string line;
-    char changer[] = " ";
-    char * chest;
-    unordered_map<string, string> m;
-    int large_counter = 0;
-    int small_counter = 0;
     file.open("data.txt", ios::in);
-    while(getline(file,line)){
-        if(line.empty()){
-            if(small_counter > 6){
-                if(checker(m) == true){
-                    large_counter++;
-                }
-                m.clear();
-                small_counter = 0;
-                continue;
-            }
-            small_counter = 0;
-            m.clear();
-            continue;
-        }
-        else{     
-            int n = line.length();
-            char str[n];
-            strcpy(str, line.c_str());
-            chest = strtok(str, changer);
-            while( chest != NULL )
-            {
-                if(chest[0] == 'c'){
-                    chest = strtok( NULL, changer);
-                    continue;
-                }
-                string letters = "";
-                string value = "";
-                for(int i=0;i<3;i++){
-                    letters += chest[i];
-                }
-                auto size = strlen(chest);
-                for(int i=4;i<size;i++){
-                    value += chest[i];
-                }
-                m.insert(make_pair(letters, value));
-                small_counter++;
-                chest = strtok( NULL, changer );
-            }   
-        }
-    }
-    if(small_counter > 6){
-                if(checker(m) == true){
-                    large_counter++;
-                }
-                m.clear();
-                small_counter = 0;
-                cout << endl;
-            }
-    cout << large_counter << endl;
+    separate_pasport(file);
+    file.close();
+    ////////////////////////////////////////////////
+
+
+
+
     return 0;
 }
