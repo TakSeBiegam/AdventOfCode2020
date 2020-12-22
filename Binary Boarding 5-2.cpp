7d6e33dc22552abffd62eff11839d28200da8d0b
TakSeBiegam <63842313+TakSeBiegam@users.noreply.github.com>
2020-12-22 20:59:33 +0100

    Update Binary Boarding 5-2.cpp

--- Binary Boarding 5-2.cpp
+++ Binary Boarding 5-2.cpp
@@ -9,15 +9,15 @@ int main()
 {
     fstream file;
     file.open("data.txt", ios::in);
-    string line;
-    vector<int> check;
+    string arrangement;
+    vector<int> seat_ids;
 
     int highest = 0;
-    while(getline(file,line)){
+    while(getline(file,arrangement)){
         int max = 127;
         int min = 0;
         for(int i=0;i<7;i++){
-            if(line[i] == 'B'){ //up
+            if(arrangement[i] == 'B'){ //up
                 min = (((max-min)/2)+min)+1;
             }
             else{ //F - low
@@ -29,21 +29,21 @@ int main()
         min = 0;
         max = 7;
         for(int i=0;i<3;i++){
-            if(line[i+7] == 'R'){
+            if(arrangement[i+7] == 'R'){
                 min = (((max-min)/2)+min)+1;
             }
             else{
                 max = (((max-min)/2)+min);
             }
         }
-        int checker = (row*8)+max;
-        check.push_back(checker);
+        int one_seat_id = (row*8)+max; //seat_id
+        seat_ids.push_back(one_seat_id);
     }
     file.close();
-    sort(check.begin(), check.end()); 
-    for(unsigned i = 1;i < check.size();i++){
-        cout << check[i] << endl;
-        if(check[i] != check[i-1]+1){
+    sort(seat_ids.begin(), seat_ids.end()); 
+    for(unsigned i = 1;i < seat_ids.size();i++){
+        cout << seat_ids[i] << endl;
+        if(seat_ids[i] != seat_ids[i-1]+1){
             return i;
         }
     }
