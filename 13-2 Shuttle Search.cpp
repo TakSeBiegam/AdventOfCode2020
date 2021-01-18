d8cb689f1d0aa3b976fa8f1f3b52c6ca0b9c2db4
TakSeBiegam <darkarek1@gmail.com>
2021-01-18 13:28:51 +0100

    not completed

--- 13-2 Shuttle Search.cpp
+++ 13-2 Shuttle Search.cpp
@@ -5,70 +5,84 @@
 
 using namespace std;
 
-void split_string(string &testcase, vector<int> &number_of_bus){
+void split_string(string &testcase, vector<int> &number_of_bus, vector<int> &timestamp_each_bus){
     string buffor = "";
-    int bus_number;
+    int bus_number = 0; //counting 'x' bus
+    int bus_station_remember;
     for (size_t i = 0; i < testcase.length()+1; i++){
         switch (testcase[i])
         {
         case ',':
-            stringstream(buffor) >> bus_number;
-            buffor = "";
+            stringstream(buffor) >> bus_station_remember;
+            buffor.clear();
+            timestamp_each_bus.push_back(bus_station_remember);
             number_of_bus.push_back(bus_number);
+            bus_number++;
             break;
         case 'x':
-            buffor = "";
+            buffor.clear();
             i++;
+            bus_number++;
             break;
         default:
             buffor += testcase[i];
             break;
         }
     }
-    stringstream(buffor) >> bus_number;
+    stringstream(buffor) >> bus_station_remember;
+    timestamp_each_bus.push_back(bus_station_remember);
     number_of_bus.push_back(bus_number);
 }
 
-void read_testcase(vector<int> &number_of_bus){
+void read_testcase(vector<int> &number_of_bus, vector<int> &timestamp_each_bus){
     string testcase = "";
     fstream file;
     file.open("testcase.txt", ios::in);
     for (size_t i = 0; i < 2; i++){
         getline(file,testcase);
     }
-    split_string(testcase, number_of_bus);
+    split_string(testcase, number_of_bus, timestamp_each_bus);
     file.close();
 }
 
-bool solution(vector<int> &number_of_bus){
-    long double counter = 1;
-    double counter_divided_numbers;
+bool solution(vector<int> &number_of_bus, vector<int> &timestamp_each_bus){ 
+    std::vector<int>::iterator it;
+    long long int timestamp = 0;
     while(true){
-        counter_divided_numbers = 0;
-        for(size_t i = 0; i < number_of_bus.size(); i++){
-            float math_mem = counter / number_of_bus[i];
-            cout << math_mem << endl;
-            if(math_mem == 0){
-                counter_divided_numbers++;
+        int counter_divided_numbers = 0;
+        it = find(number_of_bus.begin(), number_of_bus.end(), timestamp);
+        if(it != number_of_bus.end()){
+            for (size_t i = 0; i < number_of_bus.size(); i++){
+                int math_mem = timestamp % number_of_bus[i];
+                if(math_mem == 0){
+                    counter_divided_numbers++;
+                }
             }
+            int place = it - number_of_bus.begin();
+            number_of_bus[place] += timestamp_each_bus[place];
         }
         if(counter_divided_numbers >= 2){
-            cout << counter << endl;
+            cout << timestamp << endl;
             return true;
         }
-        counter++;
+        timestamp++;
     }
     return false;
 }
 
 int main(){
     vector<int> number_of_bus;
-    read_testcase(number_of_bus);
+    vector<int> timestamp_each_bus;
+    read_testcase(number_of_bus, timestamp_each_bus); 
+    /*for (size_t i = 0; i < number_of_bus.size(); i++)
+    {
+        cout << "Bus Number: " << number_of_bus[i] << " - " << timestamp_each_bus[i] << endl;
+    }*/
+    
     bool isgood = false;
-    isgood = solution(number_of_bus);
+    isgood = solution(number_of_bus, timestamp_each_bus);
     if(isgood == false){
         cout << "Something goes wrong !" << endl;
     }
-    
     return 0;
 }
